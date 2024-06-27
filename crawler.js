5299a17b82f93d95853dddb698f3d4f9b8b22558
jseo79 <joseph79seo@gmail.com>
2024-06-27 09:10:51 -0500

    cleaner code

--- crawler.js
+++ crawler.js
@@ -1,11 +1,11 @@
 const { Builder } = require('selenium-webdriver');
 let chrome = require('selenium-webdriver/chrome');
-const parser = require('./parser');
+const parseEasylist = require('./parser');
 
-const trackingScript = parser();
+const trackingScript = parseEasylist();
 
-// list of website urls to test for cookies and tracking scripts
-let urlToTest = [
+// List of website URLs to test for cookies and tracking scripts
+let urlsToTest = [
 	'https://www.cnn.com',
 	'https://www.youtube.com',
 	'https://www.selenium.dev',
@@ -16,115 +16,125 @@ let urlToTest = [
 	'https://www.stockx.com',
 ];
 
-// extension location for AdBlockPlus
-let abp = '--load-extension=/Users/josephseo/Repos/Ad-Tracker/AdBlockPlus';
+// Extension location for AdBlockPlus
+let adBlockPlusPath =
+	'--load-extension=/Users/josephseo/Repos/Ad-Tracker/AdBlockPlus';
 
-// async function to count the number of cookies with AdBlockPlus disabled
+// Function to count cookies with AdBlockPlus disabled
 async function getCookiesWithoutAdBlockPlus() {
 	let driver = await new Builder().forBrowser('chrome').build();
-	let ckCounter = 0;
-	for (let i = 0; i < urlToTest.length; i++) {
-		await driver.get(urlToTest[i]);
-		await driver.sleep(1000);
-		let cookies = await driver.manage().getCookies();
-		ckCounter += cookies.length;
-		//console.log(cookies);
-		//print the domain name of the cookies
-		// for (let j = 0; j < cookies.length; j++) {
-		// 	console.log(cookies[j].domain);
-		// }
+	let cookieCounter = 0;
+
+	try {
+		for (let url of urlsToTest) {
+			await driver.get(url);
+			await driver.sleep(1000);
+			let cookies = await driver.manage().getCookies();
+			cookieCounter += cookies.length;
+		}
+		console.log('Number of cookies without AdBlockPlus:', cookieCounter);
+	} finally {
+		await driver.quit();
 	}
-	console.log('Number of cookies without AdBlockPlus: ', ckCounter);
-	await driver.quit();
 }
 
-// async function to count the number of cookies with AdBlockPlus enabled
+// Function to count cookies with AdBlockPlus enabled
 async function getCookiesWithAdBlockPlus() {
 	let options = new chrome.Options();
-	options.addArguments(abp);
+	options.addArguments(adBlockPlusPath);
 
 	let driver = await new Builder()
 		.setChromeOptions(options)
 		.forBrowser('chrome')
 		.build();
+	let cookieCounter = 0;
 
-	let ckCounter2 = 0;
-
-	await driver.sleep(5000);
-	for (let i = 0; i < urlToTest.length; i++) {
-		await driver.get(urlToTest[i]);
-		await driver.sleep(1000);
-		let cookies = await driver.manage().getCookies();
-		ckCounter2 += cookies.length;
+	try {
+		await driver.sleep(5000);
+		for (let url of urlsToTest) {
+			await driver.get(url);
+			await driver.sleep(1000);
+			let cookies = await driver.manage().getCookies();
+			cookieCounter += cookies.length;
+		}
+		console.log('Number of cookies with AdBlockPlus:', cookieCounter);
+	} finally {
+		await driver.quit();
 	}
-	console.log('Number of cookies with AdBlockPlus: ', ckCounter2);
-	await driver.quit();
 }
 
-// async function to track scripts with AdBlockPlus disabled
-async function trackScriptWithoutABP() {
+// Function to track scripts with AdBlockPlus disabled
+async function trackScriptsWithoutAdBlockPlus() {
 	let driver = await new Builder().forBrowser('chrome').build();
-	let counter = 0;
+	let scriptCounter = 0;
 
-	for (let i = 0; i < urlToTest.length; i++) {
-		await driver.get(urlToTest[i]);
-		await driver.sleep(1000);
-		let page = await driver.getPageSource();
-		let index = 0;
-		while (index < trackingScript.length) {
-			let regex = new RegExp(trackingScript[index]);
-			if (regex.test(page)) {
-				counter++;
-				index++;
-			} else {
-				index++;
+	try {
+		for (let url of urlsToTest) {
+			await driver.get(url);
+			await driver.sleep(1000);
+			let pageSource = await driver.getPageSource();
+			for (let script of trackingScript) {
+				let regex = new RegExp(script);
+				if (regex.test(pageSource)) {
+					scriptCounter++;
+				}
 			}
 		}
+		console.log(
+			'Number of tracking scripts found without AdBlockPlus:',
+			scriptCounter
+		);
+	} finally {
+		await driver.quit();
 	}
-	console.log('Number of tracking scripts found without ABP: ', counter);
-	await driver.quit();
 }
 
-// async function to track scripts with AdBlockPlus enabled
-async function trackScriptWithABP() {
+// Function to track scripts with AdBlockPlus enabled
+async function trackScriptsWithAdBlockPlus() {
 	let options = new chrome.Options();
-	options.addArguments(abp);
+	options.addArguments(adBlockPlusPath);
+
 	let driver = await new Builder()
 		.setChromeOptions(options)
 		.forBrowser('chrome')
 		.build();
+	let scriptCounter = 0;
 
-	let scrCounter = 0; // counter for number of ad scripts blocked
-	await driver.sleep(5000);
-
-	for (let i = 0; i < urlToTest.length; i++) {
-		await driver.get(urlToTest[i]);
-		await driver.sleep(1000);
-		let page = await driver.getPageSource();
-		let index = 0;
-		while (index < trackingScript.length) {
-			let regex = new RegExp(trackingScript[index]);
-			if (regex.test(page)) {
-				scrCounter++;
-				index++;
-			} else {
-				index++;
+	try {
+		await driver.sleep(5000);
+		for (let url of urlsToTest) {
+			await driver.get(url);
+			await driver.sleep(1000);
+			let pageSource = await driver.getPageSource();
+			for (let script of trackingScript) {
+				let regex = new RegExp(script);
+				if (regex.test(pageSource)) {
+					scriptCounter++;
+				}
 			}
 		}
+		console.log(
+			'Number of tracking scripts found with AdBlockPlus:',
+			scriptCounter
+		);
+	} finally {
+		await driver.quit();
 	}
-	console.log('Number of tracking scripts found with ABP: ', scrCounter);
-	await driver.quit();
 }
 
+// Helper function to introduce delays
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 async function main() {
 	await getCookiesWithoutAdBlockPlus();
 	await sleep(5000);
 	await getCookiesWithAdBlockPlus();
 	await sleep(5000);
-	await trackScriptWithoutABP();
+	await trackScriptsWithoutAdBlockPlus();
 	await sleep(5000);
-	await trackScriptWithABP();
+	await trackScriptsWithAdBlockPlus();
 }
 
-main().then((r) => console.log('Done'));
+main()
+	.then(() => console.log('Done'))
+	.catch((err) => console.error(err));
